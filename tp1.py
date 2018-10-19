4d7db4a0d2f14594fb7227d256ade6df63f516da
afrferreira <afr.ferreira@campus.fct.unl.pt>
2018-10-19 18:12:56 +0100

    falta McNemar e bayes com err, passar em frente

--- tp1.py
+++ tp1.py
@@ -5,17 +5,14 @@ Created on Thu Oct 11 09:41:16 2018
 
 @author: andrefrf
 """
-import math
 import numpy as np
 import matplotlib.pyplot as plt
 import warnings;
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import KFold
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
-from t3_aux import poly_16features, create_plot
-import operator
+from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors.kde import KernelDensity
 
@@ -48,26 +45,31 @@ def calc_fold(feats, X,Y, train_ix, valid_ix,Cval):
 folds = 5
 kf = StratifiedKFold(n_splits = folds)
 feats=4
-Cval=1
-trainning_error = []
-validation_error = []
-for ite in range(20):
-    tr_err = va_err = 0
-    for tr_ix,va_ix in kf.split(Ytrain,Ytrain):
-        r,v = calc_fold(feats,Xtrain,Ytrain,tr_ix,va_ix,Cval)
-        tr_err += r
-        va_err += v    
-    trainning_error.append([np.log(Cval),tr_err/folds])
-    validation_error.append([np.log(Cval),va_err/folds])
-    Cval*=2
 
-trainning_error2 = np.array(trainning_error)
-validation_error2 = np.array(validation_error)
-plt.plot(trainning_error2[:,0],trainning_error2[:,1],'-r',label = 'training set')
-plt.plot(validation_error2[:,0],validation_error2[:,1],'-g', label='validation set') 
-
-plt.show()
-plt.close()
+    
+def LogisticReg():
+    minError=1
+    Cval=1
+    trainning_error = []
+    validation_error = []
+    for ite in range(20):
+        tr_err = va_err = 0
+        for tr_ix,va_ix in kf.split(Ytrain,Ytrain):
+            r,v = calc_fold(feats,Xtrain,Ytrain,tr_ix,va_ix,Cval)
+            tr_err += r
+            va_err += v    
+        trainning_error.append([np.log(Cval),tr_err/folds])
+        validation_error.append([np.log(Cval),va_err/folds])
+        Cval*=2
+    trainning_error2 = np.array(trainning_error)
+    validation_error2 = np.array(validation_error)
+    plt.plot(trainning_error2[:,0],trainning_error2[:,1],'-r',label = 'training set')
+    plt.plot(validation_error2[:,0],validation_error2[:,1],'-g', label='validation set') 
+    plt.show()
+    plt.close()
+    if(va_err<minError):
+        minError = va_err
+    return minError
 
 """K-Nearest Neighbours"""
 
@@ -78,24 +80,78 @@ def calcNeighbor(X,Y, train_ix, valid_ix,k):
     scoreVal = 1-knn.score(X[valid_ix],Y[valid_ix])
     return scoreTr,scoreVal
     
-training_error = []
-validation_error = []
-for k in range(1,39,2):
-    tr_err = va_err = 0
-    for tr_ix,va_ix in kf.split(Ytrain,Ytrain):
-        r,v = calcNeighbor(Xtrain,Ytrain,tr_ix,va_ix,k)
-        tr_err +=r
-        va_err += v
-    training_error.append([k,tr_err/folds])
-    validation_error.append([k,va_err/folds])
-    
-trainer = np.array(training_error)
-valid = np.array(validation_error)
-plt.plot(trainer[:,0],trainer[:,1],'-r')
-plt.plot(valid[:,0],valid[:,1])
-
-plt.show()
-plt.close()
+def KNearest():
+    minError = 1
+    training_error = []
+    validation_error = []
+    for k in range(1,39,2):
+        tr_err = va_err = 0
+        for tr_ix,va_ix in kf.split(Ytrain,Ytrain):
+            r,v = calcNeighbor(Xtrain,Ytrain,tr_ix,va_ix,k)
+            tr_err +=r
+            va_err += v
+        training_error.append([k,tr_err/folds])
+        validation_error.append([k,va_err/folds])
+    trainer = np.array(training_error)
+    valid = np.array(validation_error)
+    plt.plot(trainer[:,0],trainer[:,1],'-r')
+    plt.plot(valid[:,0],valid[:,1])
+    plt.show()
+    plt.close()
+    if(va_err<minError):
+        minError = va_err
+    return minError
 
 """Naive Bayes"""
 
+def scorer(log_z,z_kde,log_o,o_kde,test):
+    classes = np.zeros(test.shape[0])
+    for row in range(test.shape[0]):
+        z_sum = log_z
+        o_sum = log_o
+        for column in range(test.shape[1]):
+            z_sum = z_sum + z_kde[column].score_samples(test[row][column])
+            o_sum = o_sum + o_kde[column].score_samples(test[row][column])
+        if z_sum<o_sum:
+            classes[row]=1
+    return classes
+
+
+def kdeCalc(one,b):
+    kde=[]
+    for x in range(one.shape[1]):
+        kde.append(KernelDensity(bandwidth=b,kernel='gaussian').fit(one[:,x].reshape(one.shape[0],1)))
+    return kde
+        
+def data_split(X,Y):
+    zero_T_cat =np.array(X[Y==0,:])
+    one_T_cat = np.array(X[Y==1,:])
+    return zero_T_cat,one_T_cat
+
+def baysian(X,Y,train_ix,valid_ix,b):
+    z_train,o_train = data_split(X[train_ix],Y[train_ix])
+    z_valid,o_valid = data_split(X[valid_ix],Y[valid_ix])
+    z_kde = kdeCalc(z_train,b)
+    o_kde = kdeCalc(o_train,b)
+    tot_len = z_train.shape[0]+o_train.shape[0]
+    log_z = np.log(z_train.shape[0]/tot_len)
+    log_o = np.log(o_train.shape[0]/tot_len)
+    classified_train = scorer(log_z,z_kde, log_o , o_kde , X[train_ix])
+    classified_valid = scorer(log_z,z_kde, log_o , o_kde , X[valid_ix])
+    train_err = 1-accuracy_score(Y[train_ix] , classified_train)
+    valid_err = 1-accuracy_score(Y[valid_ix] , classified_valid)
+    return train_err , valid_err
+
+def naiveBayes():
+    minError = 0
+    training_error = []
+    validation_error = []
+    for b in range(1,100,2):
+        tr_err = va_err = 0
+        for tr_ix,va_ix in kf.split(Ytrain,Ytrain):
+            r,v = baysian(Xtrain,Ytrain,tr_ix,va_ix,(b/100))
+        
+          
+LogisticReg()
+KNearest()
+##naiveBayes()
